19469bbe7019066929d5a1ff6a35a4eb0ccf82f4
Vadim Kosarev <vadim.a.kosarev@gmail.com>
2021-05-17 21:12:38 +0500

    Kick CI

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello jenkins 66');
+  res.send('hello jenkins 67');
 });
  
 app.listen(process.env.PORT || 5000);
